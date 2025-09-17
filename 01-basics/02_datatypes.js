4773b6d753be2b649482c24ba65701af092dc31a
Kritical-exe <shrivastavakratika100@gmail.com>
2025-09-17 02:53:12 +0000

    datatypes

---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+//alert ( 3 + 3 )// we are using nodejs, not browser
+
+console.log( 3
+    +
+    3 )//code readability should be high
+ console.log("kritika" )
+
+ let name = "kritika"
+ let age =  17
+ let isLoggedIn =  false
+
+ //number=> 2 to power 53
+ //bigint=>
+ //string=> ""
+ //boolean=> true/false
+ //null=> stand alone value
+ //undefined=>
+ //symbol=> unique
+
+ //object
+ console.log (typeof undefined)// type is undefined
+ console.log (typeof null)// type is object
